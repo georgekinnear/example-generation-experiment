5b40186198a1516e90e17fec04cfbdefb905eb88
George Kinnear <G.Kinnear@ed.ac.uk>
2021-05-19 10:50:38 +0100

    script reads in data to data-clean

---
+++ 1_process_attempts_study1.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(janitor)
+
+
+
+# Make sure the working directory is set to the location of this script!
+
+
+# Read in details of the tasks
+question_details = read_csv("../data-raw/study1/FAC_LGE-tasks.csv") %>%
+  select(question_id, question_name) %>% 
+  rename(questionid = question_id)
+
+#
+# Participant info
+#
+
+# Experimental groups
+participant_groups = read_csv("../data-raw/study1/FAC_LGE-groups.csv") %>% 
+  clean_names() %>% 
+  separate(email_address, c("username", NA), sep = "@") %>% 
+  select(username, group) %>% 
+  mutate(group = str_replace_all(group, "X", "G"))
+
+# Consent
+participants = read_csv("../data-raw/study1/FAC_LGE-Course design research-grades.csv") %>% 
+  clean_names() %>% 
+  # get rid of the summary row at the end
+  filter(str_length(email_address) > 0) %>% 
+  # extract the username
+  separate(email_address, c("username", NA), sep = "@") %>%
+  # restrict only to those who consented to participate
+  filter(grade_1_00 == 1) %>% 
+  select(username) %>% 
+  # attach the info about experimental groups
+  left_join(participant_groups, by = "username")
+
+#
+# Question attempts
+#
+
+# Read in all the csv files of attempt data
+all_attempt_data =
+  list.files(path = "../data-raw/study1/",
+             pattern = "^attempts.*.csv",
+             full.names = TRUE) %>%
+  as_tibble() %>%
+  rename(csv_file_name = value) %>% 
+  mutate(
+    csv_data = purrr::map(csv_file_name, read.csv, stringsAsFactors = FALSE)
+  ) %>% 
+  unnest(cols = c(csv_data)) %>% 
+  # attach details of each question
+  left_join(question_details, by = "questionid") %>% 
+  # tidy up the table, and number each attempt at a question by each student
+  arrange(question_name, username, timecreated) %>% 
+  group_by(question_name, username) %>% 
+  mutate(
+    attempt_number = row_number()
+  ) %>% 
+  ungroup() %>% 
+  select(csv_file_name, question_name, everything())
+
+# Restrict to data from those who gave consent
+attempt_data = all_attempt_data %>% 
+  # only keep attempts by participants
+  semi_join(participants, by = "username") %>% 
+  # then attach details of which experimental group they were in
+  left_join(participants, by = "username") %>% 
+  # drop the single student who appears not to be in any group
+  filter(str_length(group) > 0)
+
+#
+# Write the processed data to .csv files
+#
+
+write_attempts_csv = function(DF) {
+  write.csv(DF,
+            paste0("data-clean/study1/processed-attempts-",unique(DF$question_name),".csv"),
+            row.names = FALSE)
+  return(DF)
+}
+attempt_data %>% 
+  group_by(question_name) %>% 
+  do(write_attempts_csv(.))
+
+# Save an RDS file so that all the previous steps can be skipped later on
+attempt_data %>%
+  write_rds("data-clean/FAC_LGE-attempt_data.rds")
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # example-generation-experiment
+
+### Data
+
+The `data-raw` folder contains files downloaded from Moodle, with a set for each study including:
+
+  * `*-tasks.csv` - with details of the Moodle question ID and our name for each question in the study,
+  * `*-groups.csv` - has the anonymous identifier of each student in the course, along with the experimental group they were assigned to,
+  * `*-grades.csv` - various files with the quiz results from Moodle,
+  * `attempts-*.csv` - each file has all the attempts at a particular question, obtained by using the Moodle custom SQL plugin to run the following query:
+  
+> SELECT qa.*, qas_last.*, u.firstname, u.lastname
+> FROM {question_attempts} qa
+> LEFT JOIN {question_attempt_steps} qas_last ON qas_last.questionattemptid = qa.id
+> /* attach another copy of qas to those rows with the most recent timecreated, using method from https://stackoverflow.com/a/28090544 */
+> LEFT JOIN {question_attempt_steps} qas_prev
+> 	ON qas_last.questionattemptid = qas_prev.questionattemptid
+> 		AND (qas_last.timecreated < qas_prev.timecreated
+> 				OR (qas_last.timecreated = qas_prev.timecreated
+> 						AND qas_last.id < qas_prev.id))
+> LEFT JOIN {user} u ON qas_last.userid = u.id
+> WHERE
+> qas_prev.timecreated IS NULL
+> AND qa.`questionid` = :question_id
+
+Since this raw data includes students who did not consent to participate in the study, it is not included in the respository. Instead, the scripts in the next section are used to filter the raw data into the `data-clean` folder, which is included in the repository.
+
+### 1. Process attempts
+
+There are two R scripts, `1_process_attempts_study1.R` and `1_process_attempts_study2.R` that do some minimal processing of the raw attempts data.
+
+The raw `attempts-*.csv` files include attempts from all students, including those who did not consent to participate in the study. So these R scripts use the info from `*-groups.csv` to filter the attempts data so that only consenting students are included.
+
+### 2. Analysis
+
+The notebooks `study1_analysis.Rmd` and `study2_analysis.Rmd` do the main experimental analyses.
+
+These generate the files `study1_ft.csv` and `study2_ft.csv` in the `data-clean` folder, containing the data used for the main analysis in each case (where each row is a student, and columns record the experimental group and the score on the outcome measure).
+
+
+TODO - detailed analysis of the attempts (e.g. range of examples given, success on the classification task, etc)
